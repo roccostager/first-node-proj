dcd429f12d3896eed616d10f3ae665365abeb77b
roccostager <99860439+roccostager@users.noreply.github.com>
2026-01-18 11:20:02 +0100

    Basic routing implemented

---
+++ index.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
+
+const routes = {
+    '/': `index.html`,
+    '/about': `about.html`,
+    '/contact-me': `contact-me.html`,
+};
+
+const server = http.createServer((request, response) => {
+    switch (request.url) {
+        case '/':
+            loadPage('index.html', 200);
+            break;
+        case '/about':
+            loadPage('about.html', 200);
+            break;
+        case '/contact-me':
+            loadPage('contact-me.html', 200);
+            break;
+        default:
+            loadPage('404.html', 404);
+    }
+    
+    function loadPage(fileName, statusCode) {
+        fs.readFile(fileName, (error, data) => {
+            if (error) {
+                response.writeHead(404, { 'Content-Type': 'text/html' });
+                response.write('404.html');
+            } else {
+                response.writeHead(statusCode, { 'Content-Type': 'text/html' });
+                response.write(data);
+            }
+            response.end();
+        });
+    }
+});
+
+server.listen(port, error => {
+    if (error) {
+        Error.log(error.message);
+    } else {
+        console.log('Server is listening on port ' + port);
+    }
+});
